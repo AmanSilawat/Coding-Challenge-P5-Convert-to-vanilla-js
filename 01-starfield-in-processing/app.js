01f0a0c42e6f623dc3d92346d0acb6dc1153f1bc
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2021-01-05 23:13:02 +0530

    Starfield in Processing Vanilla JavaScript

---
+++ 01-starfield-in-processing/app.js
@@ -0,0 +1,23 @@
+let stars = [];
+let speed;
+let starLength = 800;
+
+let cn = document.createElement('canvas');
+
+cn.width = 100;
+
+let ctx = cn.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(50, 50, 150, 150);
+document.body.appendChild(cn)
+
+console.log(ctx)
+
+function setup() {
+    // createCanvas(800, 800);
+
+}
+
+function draw() {
+    
+}
